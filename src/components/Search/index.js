9fee67f08507bb525eba377ab35009ec4a30c264
Tina Armstead <tina.armstead@gmail.com>
2021-03-30 20:17:23 -0400

    Added basic components and set them in place

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './style.css';
-import Card from 'react-bootstrap/Card';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 
-function Search() {
+function Search({ handleSearchChange }) {
     return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-          </Card.Text>
-                </Card.Body>
-            </Card>
+        <div className ="searchBar">
+           <InputGroup className="mb-3">
+    <FormControl
+      placeholder="What are you looking for?"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+    />
+    <InputGroup.Append>
+      <Button variant="outline-secondary">Search</Button>
+    </InputGroup.Append>
+  </InputGroup>
         </div>
 
     )
--- src/components/TableMain/index.js
+++ src/components/TableMain/index.js
@@ -3,41 +3,41 @@ import './style.css';
 import Table from 'react-bootstrap/Table';
 
 function TableMain() {
-    return (
-        <div>
-              <Table responsive>
-  <thead>
-    <tr>
-      <th>#</th>
-      {Array.from({ length: 12 }).map((_, index) => (
-        <th key={index}>Table heading</th>
-      ))}
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      {Array.from({ length: 12 }).map((_, index) => (
-        <td key={index}>Table cell {index}</td>
-      ))}
-    </tr>
-    <tr>
-      <td>2</td>
-      {Array.from({ length: 12 }).map((_, index) => (
-        <td key={index}>Table cell {index}</td>
-      ))}
-    </tr>
-    <tr>
-      <td>3</td>
-      {Array.from({ length: 12 }).map((_, index) => (
-        <td key={index}>Table cell {index}</td>
-      ))}
-    </tr>
-  </tbody>
-</Table>
-        </div>
-      
-    )
+  return (
+    <div>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <th key={index}>Table heading</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>2</td>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>3</td>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+
+  )
 }
 
 export default TableMain;
