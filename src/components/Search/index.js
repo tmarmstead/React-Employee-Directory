deb3b21e70b1982ec10ba2933ebbdfa18255c2ea
Tina Armstead <tina.armstead@gmail.com>
2021-04-05 18:24:09 -0400

    Moving TableMain into Search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import './style.css';
+import Table from 'react-bootstrap/Table';
 import API from '../../utils/API';
+
 
 class ApiSearchHandler extends PureComponent {
   state = {
@@ -63,11 +65,51 @@ class ApiSearchHandler extends PureComponent {
         </button>
         </div>
       </form>
-
     )
   }
 
 
 }
 
-export default ApiSearchHandler;
+class TableMain extends PureComponent {
+  render() {
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Full Name</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Image goes here</td>
+            <td>Mark Smith</td>
+            <td>555-555</td>
+            <td>@mdo</td>
+            <td>DOB</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob Smith</td>
+            <td>555-555</td>
+            <td>@fat</td>
+            <td>DOB</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Larry Smith</td>
+            <td>555-555</td>
+            <td>@twitter</td>
+            <td>DOB</td>
+          </tr>
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default { ApiSearchHandler, TableMain };
